1655b8e28cd8cd4ed44f980bebcde64f15e9a3fe
burglarhobbit <bhavyapatwa007@gmail.com>
2018-01-08 16:25:41 +0530

    Updated R-Net msmarco prepro

--- msmarco/prepro_msm.py
+++ msmarco/prepro_msm.py
@@ -222,7 +222,6 @@ def process_file(filename, data_type, word_counter, char_counter):
 		eval_examples[str(total)] = {
 			"passage_concat": passage_concat, "spans": spans, "answers": answer_texts, "uuid": source["query_id"]}
 		line = fh.readline()
-
 	random.shuffle(examples)
 	print("{} questions in total".format(len(examples)))
 		
