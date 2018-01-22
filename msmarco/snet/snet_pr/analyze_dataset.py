4885446093fed89bf35b6cc6cc3d591b6ef03d0f
burglarhobbit <bhavyapatwa007@gmail.com>
2018-01-22 19:07:46 +0530

    updated snet passage ranking

--- msmarco/snet/snet_pr/analyze_dataset.py
+++ msmarco/snet/snet_pr/analyze_dataset.py
@@ -414,6 +414,11 @@ def process_file(max_para_count, filename, data_type, word_counter, char_counter
 			"passage_concat": passage_concat, "spans": spans, "answers": answer_texts, "uuid": source["query_id"],
 			"passage_rank": passage_rank}
 		line = fh.readline()
+		if i%1000 == 0:
+			print("{} questions in total".format(len(examples)))
+			print("{} questions with empty answer".format(empty_answers))
+			print("{} questions with low rouge-l answers".format(low_rouge_l))
+			print("{} questions with multipara answers out of total valid examples".format(multi_para_answer_count))
 	random.shuffle(examples)
 	print("{} questions in total".format(len(examples)))
 	print("{} questions with empty answer".format(empty_answers))
